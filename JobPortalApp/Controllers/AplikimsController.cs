c030449dc09672b31d7c4f2981ce4fc06c77a8a2
Romela Qytyku <rqytyku2@uet.edu.al>
2020-04-27 13:45:47 +0200

    Add project files.

---
+++ JobPortalApp/Controllers/AplikimsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using JobPortalApp.Models;
+
+namespace JobPortalApp.Controllers
+{
+    public class AplikimsController : Controller
+    {
+        private JobPortalEntities db = new JobPortalEntities();
+
+        // GET: Aplikims
+        public ActionResult Index()
+        {
+            var aplikims = db.Aplikims.Include(a => a.Kompania).Include(a => a.PozicionPune);
+            return View(aplikims.ToList());
+        }
+
+        // GET: Aplikims/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aplikim aplikim = db.Aplikims.Find(id);
+            if (aplikim == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aplikim);
+        }
+
+        // GET: Aplikims/Create
+        public ActionResult Create()
+        {
+            ViewBag.Komp_Id = new SelectList(db.Kompanias, "ID_Kompani", "Emri");
+            ViewBag.Pozc_Id = new SelectList(db.PozicionPunes, "Id_Pozicion", "Emri_Poz");
+            return View();
+        }
+
+        // POST: Aplikims/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Aplikim,Pozc_Id,Komp_Id,Orari")] Aplikim aplikim)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Aplikims.Add(aplikim);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Komp_Id = new SelectList(db.Kompanias, "ID_Kompani", "Emri", aplikim.Komp_Id);
+            ViewBag.Pozc_Id = new SelectList(db.PozicionPunes, "Id_Pozicion", "Emri_Poz", aplikim.Pozc_Id);
+            return View(aplikim);
+        }
+
+        // GET: Aplikims/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aplikim aplikim = db.Aplikims.Find(id);
+            if (aplikim == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Komp_Id = new SelectList(db.Kompanias, "ID_Kompani", "Emri", aplikim.Komp_Id);
+            ViewBag.Pozc_Id = new SelectList(db.PozicionPunes, "Id_Pozicion", "Emri_Poz", aplikim.Pozc_Id);
+            return View(aplikim);
+        }
+
+        // POST: Aplikims/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Aplikim,Pozc_Id,Komp_Id,Orari")] Aplikim aplikim)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aplikim).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Komp_Id = new SelectList(db.Kompanias, "ID_Kompani", "Emri", aplikim.Komp_Id);
+            ViewBag.Pozc_Id = new SelectList(db.PozicionPunes, "Id_Pozicion", "Emri_Poz", aplikim.Pozc_Id);
+            return View(aplikim);
+        }
+
+        // GET: Aplikims/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Aplikim aplikim = db.Aplikims.Find(id);
+            if (aplikim == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aplikim);
+        }
+
+        // POST: Aplikims/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Aplikim aplikim = db.Aplikims.Find(id);
+            db.Aplikims.Remove(aplikim);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ JobPortalApp/Controllers/PozicionPunesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using JobPortalApp.Models;
+
+namespace JobPortalApp.Controllers
+{
+    public class PozicionPunesController : Controller
+    {
+        private JobPortalEntities db = new JobPortalEntities();
+
+        // GET: PozicionPunes
+        public ActionResult Index()
+        {
+            var pozicionPunes = db.PozicionPunes.Include(p => p.Kompania);
+            return View(pozicionPunes.ToList());
+        }
+
+        // GET: PozicionPunes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PozicionPune pozicionPune = db.PozicionPunes.Find(id);
+            if (pozicionPune == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pozicionPune);
+        }
+
+        // GET: PozicionPunes/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_Kompanie = new SelectList(db.Kompanias, "ID_Kompani", "Emri");
+            return View();
+        }
+
+        // POST: PozicionPunes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Pozicion,Emri_Poz,Tipi,ID_Kompanie")] PozicionPune pozicionPune)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PozicionPunes.Add(pozicionPune);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_Kompanie = new SelectList(db.Kompanias, "ID_Kompani", "Emri", pozicionPune.ID_Kompanie);
+            return View(pozicionPune);
+        }
+
+        // GET: PozicionPunes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PozicionPune pozicionPune = db.PozicionPunes.Find(id);
+            if (pozicionPune == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_Kompanie = new SelectList(db.Kompanias, "ID_Kompani", "Emri", pozicionPune.ID_Kompanie);
+            return View(pozicionPune);
+        }
+
+        // POST: PozicionPunes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Pozicion,Emri_Poz,Tipi,ID_Kompanie")] PozicionPune pozicionPune)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pozicionPune).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_Kompanie = new SelectList(db.Kompanias, "ID_Kompani", "Emri", pozicionPune.ID_Kompanie);
+            return View(pozicionPune);
+        }
+
+        // GET: PozicionPunes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PozicionPune pozicionPune = db.PozicionPunes.Find(id);
+            if (pozicionPune == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pozicionPune);
+        }
+
+        // POST: PozicionPunes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PozicionPune pozicionPune = db.PozicionPunes.Find(id);
+            db.PozicionPunes.Remove(pozicionPune);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
